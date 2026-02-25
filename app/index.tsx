3ad80e763ad4da8b57b0cf3f17ff41919aa9792d
alightcap <andy.lightcap@gmail.com>
2026-02-25 15:44:13 -0500

    busted form for score submission

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,13 @@
-import { Text, View } from "react-native";
+import React from "react";
+import { Text, TextInput, View } from "react-native";
 
 export default function Index() {
+  const teamName = "AIS";
+  const [date, onChangeDate] = React.useState("Date");
+  const [opponentName, onChangeOpponentName] = React.useState("Opponent Name");
+  const [ownScore, onChangeOwnScore] = React.useState("0");
+  const [oppScore, onChangeOppScore] = React.useState("0");
+
   return (
     <View
       style={{
@@ -9,7 +16,67 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Submit {teamName} Game</Text>
+      <View>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <Text style={{ marginRight: 5 }}>Date</Text>
+          <TextInput
+            onChangeText={onChangeDate}
+            value={date}
+            style={{ borderWidth: 1, padding: 10, borderRadius: 5 }}
+          />
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <Text style={{ marginRight: 5 }}>Opponent Name</Text>
+          <TextInput
+            onChangeText={onChangeOpponentName}
+            value={opponentName}
+            style={{ borderWidth: 1, padding: 10, borderRadius: 5 }}
+          />
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <Text style={{ marginRight: 5 }}>{teamName} Score</Text>
+          <TextInput
+            onChangeText={onChangeOwnScore}
+            value={ownScore}
+            style={{ borderWidth: 1, padding: 10, borderRadius: 5 }}
+            keyboardType="numeric"
+          />
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <Text style={{ marginRight: 5 }}>Opponent Score</Text>
+          <TextInput
+            onChangeText={onChangeOppScore}
+            value={oppScore}
+            style={{ borderWidth: 1, padding: 10, borderRadius: 5 }}
+            keyboardType="numeric"
+          />
+        </View>
+      </View>
     </View>
   );
 }
