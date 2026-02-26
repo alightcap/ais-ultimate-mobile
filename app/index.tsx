70bd9b8627b166724fe246c59537d3a91b5f03b1
alightcap <andy.lightcap@gmail.com>
2026-02-26 13:51:32 -0500

    added placeholder-data switched to team list for "home" route

--- app/index.tsx
+++ app/index.tsx
@@ -1,82 +1,25 @@
 import React from "react";
-import { Text, TextInput, View } from "react-native";
+import { FlatList, Text, View } from "react-native";
+import { teams } from "@/src/lib/placeholder-data";
+
+type TeamProps = { teamName: string };
+
+const Team = ({ teamName }: TeamProps) => (
+  <Text style={{ padding: 5, backgroundColor: "skyblue" }}>{teamName}</Text>
+);
 
 export default function Index() {
-  const teamName = "AIS";
-  const [date, onChangeDate] = React.useState("Date");
-  const [opponentName, onChangeOpponentName] = React.useState("Opponent Name");
-  const [ownScore, onChangeOwnScore] = React.useState("0");
-  const [oppScore, onChangeOppScore] = React.useState("0");
-
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
       }}
     >
-      <Text>Submit {teamName} Game</Text>
-      <View>
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "space-between",
-          }}
-        >
-          <Text style={{ marginRight: 5 }}>Date</Text>
-          <TextInput
-            onChangeText={onChangeDate}
-            value={date}
-            style={{ borderWidth: 1, padding: 10, borderRadius: 5 }}
-          />
-        </View>
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "space-between",
-          }}
-        >
-          <Text style={{ marginRight: 5 }}>Opponent Name</Text>
-          <TextInput
-            onChangeText={onChangeOpponentName}
-            value={opponentName}
-            style={{ borderWidth: 1, padding: 10, borderRadius: 5 }}
-          />
-        </View>
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "space-between",
-          }}
-        >
-          <Text style={{ marginRight: 5 }}>{teamName} Score</Text>
-          <TextInput
-            onChangeText={onChangeOwnScore}
-            value={ownScore}
-            style={{ borderWidth: 1, padding: 10, borderRadius: 5 }}
-            keyboardType="numeric"
-          />
-        </View>
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "space-between",
-          }}
-        >
-          <Text style={{ marginRight: 5 }}>Opponent Score</Text>
-          <TextInput
-            onChangeText={onChangeOppScore}
-            value={oppScore}
-            style={{ borderWidth: 1, padding: 10, borderRadius: 5 }}
-            keyboardType="numeric"
-          />
-        </View>
-      </View>
+      <FlatList
+        data={teams}
+        renderItem={({ item }) => <Team teamName={item.teamName} />}
+        // keyExtractor={(item) => item.id}
+      />
     </View>
   );
 }
---
+++ src/lib/placeholder-data.ts
@@ -0,0 +1,10 @@
+export const teams = [
+  {
+    id: 0,
+    teamName: "2026 Varsity Open AIS Eagles",
+  },
+  {
+    id: 1,
+    teamName: "2026 Junior Varsity Women's AIS Eagles",
+  },
+];
