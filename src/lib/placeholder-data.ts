fe43ab7b917e17de4d25819ce97ca1c416cbb63f
Andy Lightcap <andy.lightcap@gmial.com>
2026-03-06 19:13:35 -0500

    updated placeholder data with more realistic ids

--- src/lib/placeholder-data.ts
+++ src/lib/placeholder-data.ts
@@ -2,17 +2,17 @@ import { Team } from "./types";
 
 export const teams: Team[] = [
   {
-    id: "0",
+    id: "j0v81y1",
     name: "2026 Varsity Open AIS Eagles",
     players: [
       {
-        id: "0",
+        id: "41lye44",
         name: "Seth L",
         number: 15,
         playing: true,
       },
       {
-        id: "1",
+        id: "5yewgrb",
         name: "Calder Z",
         number: 65,
         playing: false,
@@ -21,19 +21,19 @@ export const teams: Team[] = [
     games: [],
   },
   {
-    id: "1",
+    id: "sg6uydo",
     name: "2026 Junior Varsity Women's AIS Eagles",
     players: [],
     games: [],
   },
   {
-    id: "2",
+    id: "0pk3iv9",
     name: "2025 MS Open AIS Eagles",
     players: [],
     games: [],
   },
   {
-    id: "3",
+    id: "v7f8lbf",
     name: "2024 MS Open AIS Eagles",
     players: [],
     games: [],
