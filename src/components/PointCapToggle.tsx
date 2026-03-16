190ea16a255f40b48f37a3f81a017e6fe4cfa1f1
Andy Lightcap <andy.lightcap@gmial.com>
2026-03-16 13:56:17 -0400

    simplified point cap component

--- src/components/PointCapToggle.tsx
+++ src/components/PointCapToggle.tsx
@@ -8,74 +8,29 @@ export default function PointCapToggle({
   currentPointCap: number;
   onPointCapChange: (newPointCap: number) => void;
 }) {
+  const pointCaps = [11, 13, 15, 17];
+
   return (
     <View style={ToggleStyles.container}>
-      <Pressable
-        onPress={() => onPointCapChange(11)}
-        style={[
-          ToggleStyles.button,
-          currentPointCap === 11 && ToggleStyles.activeButton,
-        ]}
-      >
-        <Text
+      {pointCaps.map((pointCap) => (
+        <Pressable
+          key={pointCap}
+          onPress={() => onPointCapChange(pointCap)}
           style={[
-            ToggleStyles.text,
-            currentPointCap === 11 && ToggleStyles.activeText,
+            ToggleStyles.button,
+            currentPointCap === pointCap && ToggleStyles.activeButton,
           ]}
         >
-          11
-        </Text>
-      </Pressable>
-      <Pressable
-        onPress={() => onPointCapChange(13)}
-        style={[
-          ToggleStyles.button,
-          currentPointCap === 13 && ToggleStyles.activeButton,
-        ]}
-      >
-        <Text
-          style={[
-            ToggleStyles.text,
-            currentPointCap === 13 && ToggleStyles.activeText,
-          ]}
-        >
-          13
-        </Text>
-      </Pressable>
-
-      <Pressable
-        onPress={() => onPointCapChange(15)}
-        style={[
-          ToggleStyles.button,
-          currentPointCap === 15 && ToggleStyles.activeButton,
-        ]}
-      >
-        <Text
-          style={[
-            ToggleStyles.text,
-            currentPointCap === 15 && ToggleStyles.activeText,
-          ]}
-        >
-          15
-        </Text>
-      </Pressable>
-
-      <Pressable
-        onPress={() => onPointCapChange(17)}
-        style={[
-          ToggleStyles.button,
-          currentPointCap === 17 && ToggleStyles.activeButton,
-        ]}
-      >
-        <Text
-          style={[
-            ToggleStyles.text,
-            currentPointCap === 17 && ToggleStyles.activeText,
-          ]}
-        >
-          17
-        </Text>
-      </Pressable>
+          <Text
+            style={[
+              ToggleStyles.text,
+              currentPointCap === pointCap && ToggleStyles.activeText,
+            ]}
+          >
+            {pointCap}
+          </Text>
+        </Pressable>
+      ))}
     </View>
   );
 }
